b9d9a602eb2eefb0e471a85049e246ff7585cd28
SixtoArrejin <arrejinsixto@gmail.com>
2026-01-20 00:53:36 -0300

    fix: remove automatic redirects in Login and Signup, handle them in AuthProvider

--- apps/mobile/app/(auth)/login/index.tsx
+++ apps/mobile/app/(auth)/login/index.tsx
@@ -1,27 +1,11 @@
 import LogIn from "@/components/Forms/Auth/LogIn";
-import { useAuth } from "@/hooks/useAuth";
-import { Redirect } from "expo-router";
 import { useRef } from "react";
-import { Animated, StyleSheet, View, ImageBackground } from "react-native";
+import { Animated, StyleSheet, ImageBackground } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { ActivityIndicator } from "react-native-paper";
 import fondoLogin from "@/assets/images/pirotecnia.png";
 
 export default function LoginScreen() {
-  const { session, loading } = useAuth();
   const fadeAnim = useRef(new Animated.Value(1)).current;
-
-  if (loading) {
-    return (
-      <View style={styles.center}>
-        <ActivityIndicator />
-      </View>
-    );
-  }
-
-  if (session) {
-    return <Redirect href="/home" />;
-  }
 
   return (
     <ImageBackground
@@ -45,5 +29,4 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
-  center: { flex: 1, justifyContent: "center", alignItems: "center" },
 });
